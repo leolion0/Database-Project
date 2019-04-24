0a2f03f7447d00e98e7b6ff347aab51f0101a13d
Kory Overbay <32309684+koverr@users.noreply.github.com>
2019-04-24 14:58:23 -0500

    Add files via upload
    Update to script

--- CoolCars.sql
+++ CoolCars.sql
@@ -1,3 +1,4 @@
+DROP SCHEMA IF EXISTS CoolCars;
 CREATE SCHEMA CoolCars;
 USE CoolCars;
 
@@ -28,6 +29,7 @@ CREATE TABLE Cars (
     Model VARCHAR (30) NOT NULL,
     Year INT NOT NULL,
     Color VARCHAR (15) NOT NULL,
+    Price INT NOT NULL,
     PRIMARY KEY (VIN),
     CONSTRAINT FK_Location_StoreID FOREIGN KEY (StoreID) REFERENCES Location(StoreID));
 
@@ -62,16 +64,57 @@ CREATE TABLE Helped_By (
 INSERT INTO Location (StoreID, Address, PhoneNumber, ManagerID)
 VALUES (1, '422 Roberts St', '(122)435-7688', 1), (2, '256 South Andes Ave', '(623)455-6113', 2), (3, '754 Main St', '(816)435-7222', 3);
 
-INSERT INTO Cars ( VIN, StoreID, CarCondition, Style, Make, Model, Year, Color )
+INSERT INTO Cars ( VIN, StoreID, CarCondition, Style, Make, Model, Year, Color, Price )
 VALUES
-	(2546, 1, 1, 'SUV', 'Cadillac', 'Escalade', 2019, 'Black'),
-    (8456, 1, 2, 'Sport', 'Mazda', 'Miata', 2015, 'Blue'),
-    (1932, 2, 1, 'Coupe', 'BMW', 'M4', 2020, 'Silver'),
-    (1957, 2, 1, 'Coupe', 'Porsche', '911 GT2 RS', 2017, 'Yellow'),
-    (9845, 3, 1, 'Sport', 'Bugatti', 'Veyron', 2012, 'Silver'),
-    (7298, 3, 2, 'Sport', 'Toyota', 'Supra', 2017, 'Silver'),
-    (9172, 4, 1, 'Sedan', 'Mitsubishi', 'Lancer Evolution', 2016, 'Red'),
-    (6721, 4, 3, 'Coupe', 'Ford', 'GT', 2017, 'Blue'),
-    (1826, 5, 4, 'Sport', 'Lamborghini', 'Centenario', 2017, 'Yellow');
+	(2546, 1, 1, 'SUV', 'Cadillac', 'Escalade', 2019, 'Black', 100),
+    (8456, 1, 2, 'Sport', 'Mazda', 'Miata', 2015, 'Blue', 60),
+    (1932, 2, 1, 'Coupe', 'BMW', 'M4', 2020, 'Silver', 90),
+    (1957, 2, -1, 'Coupe', 'Porsche', '911 GT2 RS', 2017, 'Yellow', 210),
+    (9845, 3, 1, 'Sport', 'Bugatti', 'Veyron', 2012, 'Silver', 560),
+    (7298, 3, 2, 'Sport', 'Toyota', 'Supra', 2017, 'Silver', 240),
+    (9172, 4, 1, 'Sedan', 'Mitsubishi', 'Lancer Evolution', 2016, 'Red', 50),
+    (6721, 4, 3, 'Coupe', 'Ford', 'GT', 2017, 'Blue', 220),
+    (1826, 5, -1, 'Sport', 'Lamborghini', 'Centenario', 2017, 'Yellow', 420),
+	(9374, 5, 1, 'Sport', 'Pagani', 'Zonda HP Barchetta', 2017, 'Silver', 530);
+    
+INSERT INTO User (Username, Password, Name, Age, CreditScore, License, Insurance, Balance, PhoneNumber)
+VALUES ('user1', 'pass1', 'Bob Builder', 43, 400, 1234, 5678, 0, '(816)712-3245'),
+	('user2', 'pass2', 'Dora Explora', 18, 200, 3245, 8573, 3000, '(281)343-2367'),
+    ('user3', 'pass3', 'Jimmy Neutron', 32, 350, 2024, 3951, 0, '(382)333-5973'),
+    ('user4', 'pass4', 'Fred Rogers', 51, 530, 3942, 6941, 0, '(394)359-6998'),
+    ('user5', 'pass5', 'Steve Rogers', 30, 450, 5908, 0899, 2500, '(959)844-6327'),
+    ('user6', 'pass6', 'Charlie Brown', 21, 390, 9943, 7462, 6000, '(733)573-8788'),
+    ('user7', 'pass7', 'Jack Daniels', 57, 500, 8612, 8374, 0, '(816)838-3342'),
+    ('user8', 'pass8', 'Lucile Ball', 38, 375, 8382, 7633, 1750, '(211)382-3733'),
+    ('user9', 'pass9', 'Tommy Tutone', 45, 642, 6634, 1332, 1000, '(722)867-5309'),
+    ('user10', 'pass10', 'Alec Baldwin', 56, 300, 5643, 8843, 0, '(833)356-4656');
+
+INSERT INTO Employee (EmployeeID, Password, Name, EmployeeType, StoreID, PhoneNumber)
+VALUES (1, 'password1', 'Robert Smith', 1, 1, '(281)342-2367'),
+  	(2, 'password2', 'Mark Markus', 2, 1,'(816)245-1356'),
+	(3, 'password3', 'Walter Twoshoes', 1, 2,'(913)183-1945'),
+    (4, 'password4', 'Samus Metroid', 2, 2,'(913)917-1845'),
+    (5, 'password5', 'Steven Even', 1, 3,'(913)819-1924'),
+    (6, 'password6', 'Steeeeeven Eeeeeven', 2, 3,'(816)817-1845'),
+    (7, 'password7', 'Mario Peach', 1, 4,'(816)294-1935'),
+    (8, 'password8', 'Sasquatch Hair', 2, 4,'(913)817-8495'),
+    (9, 'password9', 'Philipo Demagio', 1, 5,'(816)917-8562'),
+    (10, 'password10', 'Jacque Frenchie', 2, 5,'(913)846-1947');
+
+/*INSERT INTO Rents (VIN, Username, Duration)
+VALUES (
+*/
+CREATE VIEW AvailCars AS
+SELECT Year, Make, Model, Price, Color, Style, CarCondition, StoreID
+FROM Cars
+WHERE CarCondition != -1;
+
+
+
+SELECT *
+FROM AvailCars
+WHERE CarCondition < 3 AND Price < 200;
+
+
 
 
